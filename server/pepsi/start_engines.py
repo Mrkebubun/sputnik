4eb5519a46577713e5439dff50920ab79e7c9640
Yury Sobolev <yury.sobolev@gmail.com>
2013-09-01 17:48:16 -0400

    Removed extraneous while loop in start_engines.py.

--- server/pepsi/start_engines.py
+++ server/pepsi/start_engines.py
@@ -13,5 +13,3 @@ for contract in db_session.query(models.Contract).filter_by(active=True):
     print [contract.ticker, contract.id]
     os.system('python %s/engine.py %s %d &' % (BASE, contract.ticker, contract.id))
 
-while True:
-    time.sleep(10)
