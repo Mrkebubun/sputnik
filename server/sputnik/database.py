175e72371ebd9ee6ca6642c3377c217afe84cda3
Yury Sobolev <yury.sobolev@gmail.com>
2013-12-01 22:49:10 -0500

    Added multiuser support to leo.

--- server/sputnik/database.py
+++ server/sputnik/database.py
@@ -14,3 +14,9 @@ Base = declarative_base()
 engine = sqlalchemy.create_engine('postgresql://www-data@/www-data', echo=False)
 Session = sqlalchemy.orm.sessionmaker(bind=engine)
 #session = Session()
+
+def make_session(username="www-data"):
+    engine = sqlalchemy.create_engine("postgresql://%s@/www-data" % username, echo=False)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    return Session()
+
