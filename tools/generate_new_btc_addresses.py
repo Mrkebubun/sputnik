e1a7c4715b5fe7c5291c5689b8a8477e5ae1b61f
Sameer Parekh <sameer@creativedestruction.com>
2013-12-27 19:05:15 -0800

    add some imports

--- tools/generate_new_btc_addresses.py
+++ tools/generate_new_btc_addresses.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from sqlalchemy.orm.exc import NoResultFound
+import sys
+import os
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
     "../server"))
