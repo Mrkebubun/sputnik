e4e1c4789db9a20c6ffb650639c70ba654d5623a
Sameer Parekh <sameer@m2.io>
2014-04-08 09:32:24 -0400

    test registration

--- testing/test_ui.py
+++ testing/test_ui.py
@@ -2,6 +2,7 @@ __author__ = 'sameer'
 
 import unittest
 from selenium import webdriver
+import random, string
 
 class TestSputnik(unittest.TestCase):
     def setUp(self):
@@ -10,3 +11,16 @@ class TestSputnik(unittest.TestCase):
     def test_connect(self):
         self.driver.get('http://localhost:8888')
         self.assertEqual(self.driver.title, 'MexBT Trading Platform')
+
+    def test_register(self):
+        self.driver.get('http://localhost:8888')
+        test_username = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        test_password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        test_nickname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        self.driver.find_element_by_id("register").click()
+        self.driver.find_element_by_id("register_username").send_keys(test_username)
+        self.driver.find_element_by_id("register_password").send_keys(test_password)
+        self.driver.find_element_by_id("register_nickname").send_keys(test_nickname)
+        self.driver.find_element_by_id("register_email").send_keys("test@m2.io")
+        self.driver.find_element_by_id("register_eula").click()
+        self.driver.find_element_by_id("register_button").click()
