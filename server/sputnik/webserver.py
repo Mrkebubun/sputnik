1cafba812a42334da4b71fe47f49f4da761ee6c7
Sameer Parekh <sameer@m2.io>
2014-04-08 05:29:38 -0400

    failures should return failure.value.args

--- server/sputnik/webserver.py
+++ server/sputnik/webserver.py
@@ -201,13 +201,13 @@ class PublicInterface:
             """
             return [True, result]
 
-        def _cb_error(fail):
+        def _cb_error(failure):
             """
 
-            :param fail: The failure details from ZMQ
+            :param failure: The failure details from ZMQ
             :returns: list
             """
-            return [False, fail.value.message.message]
+            return [False, failure.value.args]
 
         d = self.factory.accountant.get_audit()
         d.addCallback(_cb)
@@ -520,13 +520,13 @@ class PepsiColaServerProtocol(WampCraServerProtocol):
                 return {"authextra": "",
                         "permissions": {"pubsub": [], "rpc": [], "username": username}}
 
-        def _cb_error(fail):
+        def _cb_error(failure):
             """
 
-            :param fail:
+            :param failure:
             :returns: dict - fake permissions so we don't leak which users exist or not
             """
-            logging.error("Unable to get permissions for %s: %s" % (username, fail.value.message.message))
+            logging.error("Unable to get permissions for %s: %s" % (username, failure.value.args))
             return {"authextra": "",
                     "permissions": {"pubsub": [], "rpc": [], "username": username}}
 
@@ -897,13 +897,13 @@ class PepsiColaServerProtocol(WampCraServerProtocol):
             """
             return [True, result]
 
-        def _cb_error(fail):
+        def _cb_error(failure):
             """
 
-            :param fail:
+            :param failure:
             :returns: list - [False, message]
             """
-            return [False, fail.value.message.message]
+            return [False, failure.value.args]
 
         d = self.factory.accountant.get_ledger_history(self.username, from_timestamp, to_timestamp)
         d.addCallback(_cb)
