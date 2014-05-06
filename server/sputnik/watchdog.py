c4a6660684d1df5220bb13ee116957c273afab32
Sameer Parekh <sameer@m2.io>
2014-05-06 13:52:58 -0400

    fix up the watchdog per yury's comments

--- server/sputnik/watchdog.py
+++ server/sputnik/watchdog.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
 import zmq_util
-from twisted.application.internet import TimerService
 from twisted.internet import reactor
-from datetime import datetime, timedelta
+from datetime import datetime
 import logging
 import config
 from alerts import send_alert
 
 class WatchdogExport(object):
     @zmq_util.export
-    def ping(self, id):
-        return id
+    def ping(self):
+        return "pong"
 
 def watchdog(address):
     return zmq_util.router_share_async(WatchdogExport(), address)
@@ -20,14 +19,12 @@ class Watchdog(object):
     def __init__(self, name, address, step=60):
         self.process = zmq_util.dealer_proxy_async(address)
         self.name = name
-        self.step_timedelta = timedelta(seconds=step)
-        self.timer_service = TimerService(step, self.ping)
-        self.next_ping_id = 0
-        self.ping_times = {}
+        self.step = step
+        self.last_ping_time = None
 
-    def got_ping(self, id):
-        gap = datetime.utcnow() - self.ping_times[id]
-        logging.info("%s ping %d received: %f ms" % (self.name, id, gap.total_seconds() * 1000))
+    def got_ping(self, event=None):
+        gap = datetime.utcnow() - self.last_ping_time
+        logging.info("%s ping received: %f ms" % (self.name, gap.total_seconds() * 1000))
         if gap.total_seconds() > 0.1:
             send_alert("%s lag > 100ms: %f ms" % (self.name, gap.total_seconds() * 1000))
 
@@ -35,13 +32,17 @@ class Watchdog(object):
         send_alert("%s ping error: %s" % (self.name, error))
 
     def ping(self):
-        self.ping_times[self.next_ping_id] = datetime.utcnow()
-        d = self.process.ping(self.next_ping_id)
-        self.next_ping_id += 1
+        self.last_ping_time = datetime.utcnow()
+        d = self.process.ping()
         d.addCallbacks(self.got_ping, self.ping_error)
+        d.addCallback(self.schedule_ping)
+
+    def schedule_ping(self, event=None):
+        reactor.callLater(self.step, self.ping)
 
     def run(self):
-        self.timer_service.startService()
+        logging.info("Watchdog %s starting" % self.name)
+        self.schedule_ping()
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s() %(lineno)d:\t %(message)s', level=logging.INFO)
