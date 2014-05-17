5c6d472c3c30547ce57598cd844e9769ce2c94eb
Sameer Parekh <sameer@m2.io>
2014-05-17 16:14:33 -0400

    resolve unclean reactor test problem. Does recreating the push_proxy_async every time send_alert is called have a serious performance impact? send_alert shouldn't be called very often...

--- server/sputnik/alerts.py
+++ server/sputnik/alerts.py
@@ -32,8 +32,9 @@ class AlertsExport(object):
     def send_alert(self, message, subject):
         self.alerts.send_alert(message, subject)
 
-alerts_push = push_proxy_async(config.get("alerts", "export"))
+
 def send_alert(message, subject):
+    alerts_push = push_proxy_async(config.get("alerts", "export"))
     program = os.path.basename(main.__file__)
     alerts_push.send_alert(message, "%s: %s" % (program, subject))
 
