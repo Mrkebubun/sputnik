7e50c66700b25ff1fefe5d1f456ac2bd3bbf4c30
Sameer Parekh <sameer@m2.io>
2014-05-29 17:13:30 -0400

    add coverage reporting to daily tests

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -2,11 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-git pull -u origin >/dev/null
+git pull -u origin >/dev/null 2>/dev/null
 # For now just test the non-UI stuff until we get selenium installed at sputnikmkt.com
+make clean >/dev/null
 make -k no_ui > /tmp/test_output.$$ 2>/dev/null
 if [ $? -ne 0 ]; then
   cat /tmp/test_output.$$
+else
+  make report >/dev/null
 fi
 rm /tmp/test_output.$$
 exit $?
