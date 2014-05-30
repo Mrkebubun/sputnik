79f733734fbcc13cd4168df92e83853b2a8ba94c
Sameer Parekh <sameer@m2.io>
2014-05-29 20:54:03 -0400

    remove redundancy

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -5,7 +5,7 @@ cd $DIR
 git pull -u origin >/dev/null 2>/dev/null
 # For now just test the non-UI stuff until we get selenium installed at sputnikmkt.com
 make clean >/dev/null
-make -k no_ui_report > /tmp/test_output.$$ 2>/dev/null
+make -k no_ui report > /tmp/test_output.$$ 2>/dev/null
 if [ $? -ne 0 ]; then
   cat /tmp/test_output.$$
 fi
