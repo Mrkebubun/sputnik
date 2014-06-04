9ed6a6cb5ee34ca9ee790b5d3d9cd8245c60d254
Sameer Parekh <sameer@m2.io>
2014-06-04 06:02:52 -0400

    redirect errors

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -5,10 +5,11 @@ cd $DIR
 git pull -u origin >/dev/null 2>/dev/null
 # For now just test the non-UI stuff until we get selenium installed at sputnikmkt.com
 make clean >/dev/null
-make -k no_ui report > /tmp/test_output.$$ 2>/dev/null
+make -k no_ui report >/tmp/test_output.$$ 2>/tmp/test_errors.$$
 if [ $? -ne 0 ]; then
   cat /tmp/test_output.$$
+  cat /tmp/test_errors.$$
 fi
-rm /tmp/test_output.$$
+rm /tmp/test_output.$$ /tmp/test_errors.$$
 exit $?
 
