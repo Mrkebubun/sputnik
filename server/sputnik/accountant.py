dc4eccee6659effcafece94a9b12baad6178f1e1
Yury Sobolev <yury@m2.io>
2014-07-30 16:54:12 -0400

    Fix cancel_order call.

--- server/sputnik/accountant.py
+++ server/sputnik/accountant.py
@@ -529,7 +529,7 @@ class Accountant:
     def raiseException(self, failure):
         raise failure.value
 
-    def cancel_order(self, order_id, username=None):
+    def cancel_order(self, username, order_id):
         """Cancel an order by id.
 
         :param id: The order id to cancel
