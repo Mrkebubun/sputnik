450297332549d53aa247418f6b79a73aa1ecf3ef
Yury Sobolev <yury@m2.io>
2014-08-05 21:38:22 -0400

    Added back implicit validation.

--- server/sputnik/rpc_schema.py
+++ server/sputnik/rpc_schema.py
@@ -21,28 +21,54 @@ def validate(x, full_uri):
     validator = jsonschema.Draft4Validator(schema, resolver=resolver)
     validator.validate(x)
 
+
+def validator(full_uri):
+    uri, fragment = jsonschema.compat.urldefrag(full_uri)
+    schema_root = config.get("specs", "schema_root")
+    with open(os.path.join(schema_root, uri)) as schema_file:
+        top_schema = json.load(schema_file)
+    resolver = jsonschema.RefResolver("file://" + schema_root + "/", top_schema)
+    schema = resolver.resolve_fragment(top_schema, fragment)
+    jsonschema.Draft4Validator.check_schema(schema)
+    return jsonschema.Draft4Validator(schema, resolver=resolver)
+
 def schema(path):
     def wrap(f):
         f.schema = path
-        return f
+        f.validator = validator(path)
+        if inspect.ismethod(f):
+            def wrapped_f(*args, **kwargs):
+                validate_method(f, *args, **kwargs)
+                f(*args, **kwargs)
+        else:
+            def wrapped_f(*args, **kwargs):
+                validate_function(f, *args, **kwargs)
+                f(*args, **kwargs)
+        return wrapped_f
     return wrap
 
-def validate_call(f, *args, **kwargs):
-    if not hasattr(f, "schema"):
-        raise RPCSchemaException("No schema associated with method.")
-
-    if inspect.ismethod(f):
-        callargs = inspect.getcallargs(f, None, *args, **kwargs)
-        del callargs["self"]
-    else:
-        callargs = inspect.getcallargs(f, *args, **kwargs)
+def validate_function(f, *args, **kwargs):
+    callargs = inspect.getcallargs(f, *args, **kwargs)
 
     # json only accepts lists as arrays, not tuples
     for key in callargs:
         if type(callargs[key]) == tuple:
             callargs[key] = list(callargs[key])
 
-    validate(callargs, f.schema)
+    f.validator.validate(callargs)
+
+
+def validate_method(f, *args, **kwargs):
+    callargs = inspect.getcallargs(f, None, *args, **kwargs)
+    del callargs["self"]
+
+    # json only accepts lists as arrays, not tuples
+    for key in callargs:
+        if type(callargs[key]) == tuple:
+            callargs[key] = list(callargs[key])
+    
+    f.validator.validate(callargs)
+
 
 @schema("rpc/accountant.json#place_order")
 def place_order(username, order):
@@ -51,6 +77,5 @@ def place_order(username, order):
 if __name__ == "__main__":
     order = {"username":"foo", "price":1000, "quantity":1, "ticker":"BTC1401", "side":"BUY", "timestamp":1407213633486125}
 
-    validate(order, "objects/order.json")
-    validate_call(place_order, "foo", order)
+    place_order("foo", order)
 
