3bfa57cc71c0c2176829c82d6ca9a50b2d764d6b
Yury Sobolev <yury@m2.io>
2014-08-20 14:01:51 -0400

    Started util.position_calculated.

--- server/sputnik/util.py
+++ server/sputnik/util.py
@@ -171,6 +171,14 @@ def get_contract(session, ticker):
     except NoResultFound:
         raise Exception("Could not resolve contract '%s'." % ticker)
 
+def position_calculated(position, session):
+    start = position.position_cp_timestamp or 0
+    checkpoint = position.position_checkpoint or 0
+    postings = session.query(models.Posting).filter_by(username=position.username).filter_by(contract_id=position.contract_id).filter_by(timestamp>start)
+    # TODO: we can actually sum this in SQL itself
+    calculated = sum([posting.quantity for posting in postings])
+    return checkpoint + calculated
+
 class ChainedOpenSSLContextFactory(ssl.DefaultOpenSSLContextFactory):
     def __init__(self, privateKeyFileName, certificateChainFileName,
                  sslmethod=SSL.SSLv23_METHOD):
