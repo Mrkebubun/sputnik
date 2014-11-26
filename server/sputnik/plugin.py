886546096c3491f3237abf481440ba410bf83667
Yury Sobolev <yury@m2.io>
2014-11-26 00:38:38 -0500

    Add plugin system.

---
+++ server/sputnik/plugin.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+from twisted.internet.defer import inlineCallbacks
+
+import observatory
+
+debug, log, warn, error, critical = observatory.get_loggers("plugin_manager")
+
+class PluginException(Exception):
+    pass
+
+class PluginManager:
+    def __init__(self):
+        self.plugins = {}
+        self.services = defaultdict(list)
+
+    @inlineCallbacks
+    def load_plugin(self, plugin):
+        debug("Loading plugin %s..." % plugin.name)
+        if plugin.name in self.plugins:
+            warn("Plugin %s already loaded." % plugin.name)
+            return
+        debug("Configuring plugin %s..." % plugin.name)
+        try:
+            plugin.configure(self)
+            # wait until plugin is done
+            yield plugin.init()
+        except Exception, e:
+            error("Unable to load plugin %s." % plugin.name)
+            error()
+            raise PluginException("Unable to load plugin %s." % plugin.name)
+        debug("Plugin %s loaded." % plugin.name)
+        self.plugins[plugin.name] = plugin
+        self.services[plugin.service].append(plugin)
+
+    @inlineCallbacks
+    def unload_plugin(self, plugin):
+        warn("Plugin unloading not officially supported.")
+        debug("Unloading plugin %s..." % plugin.name)
+        if plugin.name not in self.plugins:
+            warn("Plugin %s not loaded." % plugin.name)
+            return
+        debug("Deconfiguring plugin %s..." % plugin.name)
+        # wait until plugin is done
+        try:
+            yield plugin.shutdown()
+        except Exception, e:
+            error("Unable to unload plugin %s." % plugin.name)
+            error()
+            raise PluginException("Unable to unload plugin %s." % plugin.name)
+        finally:
+            if plugin.name in self.plugins:
+                del self.plugins[plugin.name]
+            if plugin in self.services[plugin.service]:
+                self.services[plugin.service].remove(plugin)
+        debug("Plugin %s unloaded." % plugin.name)
+
+class Plugin:
+    def __init__(self, name, service):
+        self.name = name
+        self.service = service
+
+    def configure(self, manager):
+        self.manager = manager
+
+    def init(self):
+        pass
+
+    def shutdown(self):
+        pass
+
