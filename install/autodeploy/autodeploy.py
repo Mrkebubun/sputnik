35b1a7bcc17b90f597028d51ef995344bd644d80
Sameer Parekh Brenn <sameer@m2.io>
2014-11-25 20:18:17 -0500

    query should spit out the tag too

--- install/autodeploy/autodeploy.py
+++ install/autodeploy/autodeploy.py
@@ -539,7 +539,7 @@ class Instance:
 
         parser = ConfigParser.SafeConfigParser()
         parser.readfp(cStringIO.StringIO(result))
-        print "Git hash: %s" % parser.get("version", "git_hash")
+        print "%s: %s" % (parser.get("version", "git_tag"), parser.get("version", "git_hash"))
 
     def login(self):
         if self.broken:
