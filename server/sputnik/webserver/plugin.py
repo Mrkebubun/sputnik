62b53173de0b0ac9578ee4ecb63bf70374d2cf61
Yury Sobolev <yury@sputnikmkt.com>
2014-12-03 15:24:16 -0500

    Add default onJoin behavior.

--- server/sputnik/webserver/plugin.py
+++ server/sputnik/webserver/plugin.py
@@ -1,5 +1,6 @@
 from sputnik.plugin import Plugin
 from autobahn.twisted.wamp import ApplicationSession
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 class AuthenticationPlugin(Plugin):
     def onHello(self, router_session, realm, details):
@@ -22,6 +23,16 @@ class SchemaPlugin(Plugin):
 class ServicePlugin(Plugin, ApplicationSession):
     def __init__(self):
         ApplicationSession.__init__(self)
+    
+    @inlineCallbacks
+    def onJoin(self, details):
+        results = yield self.register(self)
+        for success, result in results:
+            if success:
+                log("Registered %s." % self._registrations[result.id].procedure)
+            else:
+                error("Error registering method: %s." % result.value.args[0])
+
 
 class DatabasePlugin(Plugin):
     pass
