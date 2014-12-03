6e8d3949c58afcf6d5d24c5a9d7740ffba829924
Yury Sobolev <yury@sputnikmkt.com>
2014-12-03 16:11:07 -0500

    Move plugin initialization to after all plugins are loaded.

--- server/sputnik/plugin.py
+++ server/sputnik/plugin.py
@@ -13,7 +13,6 @@ class PluginManager:
         self.plugins = {}
         self.services = defaultdict(list)
 
-    @inlineCallbacks
     def load(self, path):
         module_name, class_name = path.rsplit(".", 1)
         mod = __import__(module_name)
@@ -25,12 +24,22 @@ class PluginManager:
         plugin.service_name = module_name.rsplit(".", 1)[0]
         plugin.class_name = class_name
         plugin.plugin_path = path
-        returnValue((yield self._load_plugin(plugin)))
+        plugin.manager = self
+        return self._load_plugin(plugin)
+
+    @inlineCallbacks
+    def init(self, plugin):
+        plugin = yield self._init_plugin(plugin)
+        returnValue(plugin)
+
+    @inlineCallbacks
+    def shutdown(self, plugin):
+        plugin = yield self._shutdown_plugin(plugin)
+        returnValue(plugin)
 
     def unload(self, path):
         return self._unload_plugin(self.plugins[path])
 
-    @inlineCallbacks
     def _load_plugin(self, plugin):
         path = plugin.plugin_path
         debug("Loading plugin %s..." % path)
@@ -38,48 +47,54 @@ class PluginManager:
             warn("Plugin %s already loaded." % path)
             return
         debug("Configuring plugin %s..." % path)
-        try:
-            plugin.configure(self)
-            # wait until plugin is done
-            yield plugin.init()
-        except Exception, e:
-            error("Unable to load plugin %s." % path)
-            error()
-            raise PluginException("Unable to load plugin %s." % path)
+        plugin.configure()
         debug("Plugin %s loaded." % path)
         self.plugins[path] = plugin
         self.services[plugin.service_name].append(plugin)
+        return plugin
+
+    @inlineCallbacks
+    def _init_plugin(self, plugin):
+        path = plugin.plugin_path
+        debug("Initializing plugin %s..." % path)
+        try:
+            yield plugin.init()
+        except Exception, e:
+            error("Unable to initialize plugin %s." % path)
+            error(e)
+            raise PluginException("Unable to initialize plugin %s." % path)
+        debug("Plugin %s is ready." % path)
         returnValue(plugin)
 
     @inlineCallbacks
+    def _shutdown_plugin(self, plugin):
+        path = plugin.plugin_path
+        debug("Shutting down plugin %s..." % path)
+        try:
+            yield plugin.shutdown()
+        except Exception, e:
+            error("Unable to shut down plugin %s." % path)
+            error(e)
+            raise PluginException("Unable to shut down plugin %s." % path)
+        debug("Plugin %s is done." % path)
+        returnValue(plugin)
+
     def _unload_plugin(self, plugin):
-        warn("There is no guarantee module code is completely removed.")
         path = plugin.plugin_path
         debug("Unloading plugin %s..." % path)
         if path not in self.plugins:
             warn("Plugin %s not loaded." % path)
             return
-        debug("Deconfiguring plugin %s..." % path)
-        # wait until plugin is done
-        try:
-            yield plugin.shutdown()
-        except Exception, e:
-            error("Unable to unload plugin %s." % path)
-            error()
-            raise PluginException("Unable to unload plugin %s." % path)
-        finally:
-            if path in self.plugins:
-                del self.plugins[path]
-            if plugin in self.services[plugin.service_name]:
-                self.services[plugin.service_name].remove(plugin)
+        if plugin in self.services[plugin.service_name]:
+            self.services[plugin.service_name].remove(plugin)
         debug("Plugin %s unloaded." % path)
 
 class Plugin:
     def __init__(self):
         pass
 
-    def configure(self, manager):
-        self.manager = manager
+    def configure(self):
+        pass
 
     def init(self):
         pass
@@ -89,20 +104,29 @@ class Plugin:
 
 def run_with_plugins(plugin_paths, callback, *args, **kwargs):
     plugin_manager = PluginManager()
+    plugins = [plugin_manager.load(plugin_path) for plugin_path in plugin_paths]
     deferreds = []
-    for plugin_path in plugin_paths:
-        deferreds.append(plugin_manager.load(plugin_path))
+    for plugin in plugins:
+        deferreds.append(plugin_manager.init(plugin))
     dl = DeferredList(deferreds)
     def run_callback(result):
         for success, value in result:
             if not success:
+                error("Not all plugins loaded. Aborting.")
                 return
         callback(plugin_manager, *args, **kwargs)
+    @inlineCallbacks
     def cleanup(_):
+        plugins.reverse()
         plugin_paths.reverse()
+        for plugin in plugins:
+            try:
+                yield plugin_manager.shutdown(plugin)
+            except Exception, e:
+                pass
         for plugin_path in plugin_paths:
             plugin_manager.unload(plugin_path)
-        return _
+        returnValue(_)
     dl.addCallback(run_callback)
     dl.addBoth(cleanup)
     dl.addErrback(error)
