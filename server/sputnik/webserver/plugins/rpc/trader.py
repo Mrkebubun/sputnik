ff35f021e41f3dfa15246393d3e610c1b214382f
Sameer Parekh <sameer@m2.io>
2014-12-12 18:28:24 -0700

    add place_order

--- server/sputnik/webserver/plugins/rpc/trader.py
+++ server/sputnik/webserver/plugins/rpc/trader.py
@@ -5,6 +5,8 @@ debug, log, warn, error, critical = observatory.get_loggers("trader")
 
 from sputnik.plugin import PluginException
 from sputnik.webserver.plugin import ServicePlugin
+from sputnik import util
+import datetime
 
 from twisted.internet.defer import inlineCallbacks, returnValue
 from autobahn import wamp
@@ -12,14 +14,36 @@ from autobahn.wamp.types import RegisterOptions
 
 
 class TraderService(ServicePlugin):
+    MAX_TICKER_LENGTH = 100
+
     def __init__(self):
         ServicePlugin.__init__(self)
 
     def init(self):
         self.accountant = self.require("sputnik.webserver.plugins.backend.accountant.AccountantProxy")
 
-    def place_order(self, ticker, price, quantity, details):
-        log(details)
+    @wamp.register(u"rpc.trader.place_order")
+    @inlineCallbacks
+    def place_order(self, order, details):
+        order['contract'] = order['contract'][:self.MAX_TICKER_LENGTH]
+        order["timestamp"] = util.dt_to_timestamp(datetime.datetime.utcnow())
+        order['username'] = details.authid
+        order["price"] = int(order["price"])
+        order["quantity"] = int(order["quantity"])
+
+        # Check for zero price or quantity
+        if order["price"] == 0 or order["quantity"] == 0:
+            returnValue([False, "exceptions/webserver/invalid_price_quantity"])
+
+        # check tick size and lot size in the accountant, not here
+
+
+        try:
+            result = yield self.accountant.proxy.place_order(details.authid, order)
+            returnValue([True, result])
+        except Exception as e:
+            returnValue([False, e.args])
+
 
     @inlineCallbacks
     def onJoin(self, details):
