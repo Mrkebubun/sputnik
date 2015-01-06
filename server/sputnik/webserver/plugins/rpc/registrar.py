a6f9c2e2e2e2e5d89be16b03e0d44632f5fa893c
Yury Sobolev <yury@m2.io>
2015-01-06 02:49:13 -0500

    Move cookie services.

--- server/sputnik/webserver/plugins/rpc/registrar.py
+++ server/sputnik/webserver/plugins/rpc/registrar.py
@@ -3,7 +3,7 @@ from sputnik import observatory
 
 debug, log, warn, error, critical = observatory.get_loggers("rpc_registrar")
 
-from sputnik.plugin import PluginException, authenticated
+from sputnik.plugin import PluginException
 from sputnik.webserver.plugin import ServicePlugin
 
 from twisted.internet.defer import inlineCallbacks, returnValue
@@ -56,18 +56,4 @@ class RegistrarService(ServicePlugin):
                     username)
             error(e)
             returnValue([False, e.args])
-
-    @wamp.register(u"rpc.registrar.get_cookie")
-    @authenticated
-    def get_cookie(self, username):
-        cookie = self.cookie_jar.get_cookie(username)
-        if cookie is None:
-            return self.cookie_jar.new_cookie(username)
-        return cookie
-
-    @wamp.register(u"rpc.registrar.logout")
-    @authenticated
-    def logout(self, username):
-        self.cookie_jar.delete_cookie(username)
-        # TODO: disconnect here
 
---
+++ server/sputnik/webserver/plugins/rpc/token.py
@@ -0,0 +1,38 @@
+from sputnik import config
+from sputnik import observatory
+
+debug, log, warn, error, critical = observatory.get_loggers("rpc_token")
+
+from sputnik.plugin import PluginException, authenticated
+from sputnik.webserver.plugin import ServicePlugin
+
+from twisted.internet.defer import inlineCallbacks, returnValue
+from autobahn import wamp
+
+class TokenService(ServicePlugin):
+    def __init__(self):
+        ServicePlugin.__init__(self)
+
+    def init(self):
+        self.administrator = self.require("sputnik.webserver.plugins.backend.administrator.AdministratorProxy")
+        self.cookie_jar = self.require("sputnik.webserver.plugins.authn.cookie.CookieAuth")
+    
+    @wamp.register(u"rpc.registrar.get_cookie")
+    @authenticated
+    def get_cookie(self, username):
+        cookie = self.cookie_jar.get_cookie(username)
+        if cookie is None:
+            return self.cookie_jar.new_cookie(username)
+        return cookie
+
+    @wamp.register(u"rpc.registrar.logout")
+    @authenticated
+    def logout(self, username):
+        self.cookie_jar.delete_cookie(username)
+        # TODO: disconnect here
+
+    @inlineCallbacks
+    def register(self, endpoint, procedure = None, options = None):
+        results = yield ServicePlugin.register(self, endpoint, procedure, options=RegisterOptions(details_arg="details", discloseCaller=True))
+        returnValue(results)
+
