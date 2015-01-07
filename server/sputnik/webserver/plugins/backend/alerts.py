c337e6ffae15f56bfc4ead42156038268de70201
Sameer Parekh <sameer@m2.io>
2015-01-07 02:39:17 -0700

    add plugins

--- server/sputnik/webserver/plugins/backend/alerts.py
+++ server/sputnik/webserver/plugins/backend/alerts.py
@@ -4,11 +4,11 @@ from sputnik import observatory
 debug, log, warn, error, critical = observatory.get_loggers("alerts_proxy")
 
 from sputnik.webserver.plugin import BackendPlugin
-from sputnik.alerts import AlertsProxy
+from sputnik.alerts import AlertsProxy as ap
 
 class AlertsProxy(BackendPlugin):
     def __init__(self):
         BackendPlugin.__init__(self)
-        self.proxy = AlertsProxy(config.get("alerts", "export"))
+        self.proxy = ap(config.get("alerts", "export"))
 
 
