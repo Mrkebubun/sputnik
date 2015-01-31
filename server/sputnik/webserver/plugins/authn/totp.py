1b7f11be55773f0702439ba375c872994142355a
Yury Sobolev <yury@m2.io>
2015-01-30 20:43:31 -0500

    Update plugin totp code.

--- server/sputnik/webserver/plugins/authn/totp.py
+++ server/sputnik/webserver/plugins/authn/totp.py
@@ -11,6 +11,9 @@ class TOTPVerification(AuthenticationPlugin):
     def __init__(self):
         AuthenticationPlugin.__init__(self)
 
+    def init(self):
+        self.administrator = self.require("sputnik.webserver.plugins.backend.administrator.AdministratorProxy")
+
     @inlineCallbacks
     def onHello(self, router_session, realm, details):
         for authmethod in details.authmethods:
@@ -26,21 +29,24 @@ class TOTPVerification(AuthenticationPlugin):
                     for db in databases:
                         result = yield db.lookup(username)
                         if result is not None:
-                            router_session.totp = result['totp']
+                            router_session.totp = result['totp_enabled']
                             break
                 except Exception, e:
                     error("Caught exception looking up user.")
                     error()
 
+    @inlineCallbacks
     def onAuthenticate(self, router_session, signature, extra):
         if not hasattr(router_session, "totp"):
             return
 
+        username = router_session.challenge["authid"].encode("utf8")
         totp = router_session.totp
         if totp:
             if "totp" not in extra:
-                return types.Deny(message=u"Missing TOTP.")
-            codes = [auth.compute_totp(totp, i) for i in range(-1, 2)]
-            if extra["totp"].encode("utf8") not in codes:
-                return types.Deny(message=u"Invalid TOTP.")
+                returnValue(types.Deny(message=u"Missing TOTP."))
+            success = yield self.administrator.proxy.check_totp(
+                    username, extra["totp"].encode("utf-8"))
+            if not success: 
+                returnValue(types.Deny(message=u"Invalid TOTP."))
 
