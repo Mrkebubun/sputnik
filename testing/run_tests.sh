18eb3823da101a3cdaaee3e464898a833b09530b
Sameer Parekh <sameer@m2.io>
2015-02-27 13:38:02 -0700

    closes #661

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 git pull -u origin >/dev/null 2>/dev/null
-(cd ..;git submodule update)
+(cd ..;git submodule update) >/dev/null 2>/dev/null
 # For now just test the non-UI stuff until we get selenium installed at sputnikmkt.com
 make clean >/dev/null
 make -k no_ui report >/tmp/test_output.$$ 2>/tmp/test_errors.$$
