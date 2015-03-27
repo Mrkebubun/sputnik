28087f7c4dc1a5c38aa5314770480b9e646019db
Sameer Brenn <sameer@m2.io>
2015-03-27 17:00:10 -0600

    add mpl to all files

--- tools/check-keys.sh
+++ tools/check-keys.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Copyright 2014 Mimetic Markets, Inc.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 REVS=`git rev-list HEAD`
 for rev in $REVS; do
     git checkout $rev
